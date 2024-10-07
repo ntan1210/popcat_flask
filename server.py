a751c7b6f1589185da08869cb8e0b46e06214b8c
An Nguyễn <ntan1210@MacBook-Pro-cua-An.local>
2024-10-08 06:08:32 +0700

    m

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 CORS(app)
 
 # Global click counter
-total_clicks = 0
+total_clicks = 210211
 
 
 # Endpoint to get the current click count
